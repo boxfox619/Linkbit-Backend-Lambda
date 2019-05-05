e8669c6a4b67a78dc276750f1e6885b7279aeb9c
boxfox619 <boxfoxsg619@gmail.com>
2019-05-05 17:20:00 +0900

    refactoring random token generator

--- util/randomToken.ts
+++ util/randomToken.ts
@@ -1,9 +1,9 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
 export default function randomToken(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    let result = '';
+    for ( let i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
  }
