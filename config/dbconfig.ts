7a2ca79279e1b3f5fdaceedb7aee4fdcc0c10e60
boxfox619 <boxfoxsg619@gmail.com>
2019-05-12 23:14:26 +0900

    fix to getting db config from env

---
+++ config/dbconfig.ts
@@ -0,0 +1,12 @@
+import { Options } from "sequelize/types";
+
+export const config: Options = {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT || 5432),
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+}
+
+export default config;
