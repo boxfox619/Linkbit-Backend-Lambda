d06ddfaac55d6bed7d263f1c95ea7cf040db0db9
boxfox619 <boxfoxsg619@gmail.com>
2019-05-14 00:19:53 +0900

    Fix certification cache to repository

--- handler/certification.ts
+++ handler/certification.ts
@@ -1,18 +1,16 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import * as cert from '../service/certificationCache';
-import { middleware, RequiredParams } from '../util/middleware';
+import { CertificationRepository }from '../service/certificationRepository';
+import { middleware } from '../util/middleware';
 import { response } from '../models';
 
-const requiredParam: RequiredParams = {
-  queryParams: ['publicKey']
-}
+const certificationRepo = new CertificationRepository();
 
 export const getCertText: APIGatewayProxyHandler = middleware(
   async (param) => {
     const publicKey = param.queryParams.publicKey;
-    const token = await cert.createCertText(publicKey);
+    const token = await certificationRepo.createCertText(publicKey);
     return response(200, { token });
   },
-  requiredParam
+  { queryParams: ['publicKey'] }
 ) 
