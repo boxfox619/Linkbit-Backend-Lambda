425539a65e7e72af63f73122ee653908401eb3cc
boxfox619 <boxfoxsg619@gmail.com>
2019-05-16 08:37:42 +0900

    Fix certification repository test

--- service/__tests__/certificationRepository.test.ts
+++ service/__tests__/certificationRepository.test.ts
@@ -1,22 +1,32 @@
-import {CertificationRepository} from '../certificationRepository';
+import { CertificationRepository } from '../certificationRepository';
+import sinon from 'sinon';
 import * as EthCrypto from 'eth-crypto';
+import AWS from 'aws-sdk';
 
 describe('certificationCache', () => {
+    const sandbox = sinon.createSandbox();
+    let putQuery;
+    const dbClient = new AWS.DynamoDB.DocumentClient();
+    const repo = new CertificationRepository(dbClient);
     const bob = EthCrypto.createIdentity();
-    const repo = new CertificationRepository();
-    let token:string = undefined;
+    let token: string = undefined;
+
     it('should generate encrypted text', async () => {
+        const updateQuery = () => putQuery = putStub.getCall(0).args[0];
+        const putStub = sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'put').returns({ promise: updateQuery});
         token = await repo.createCertText(bob.publicKey);
         expect(token).toBeDefined();
     });
 
     it('should validate text', async () => {
+        sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'get').returns({ promise: () => putQuery });
+        sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'delete').returns({ promise: () => { } });
         const encrypted = EthCrypto.cipher.parse(token);
         const decryptedText = await EthCrypto.decryptWithPrivateKey(bob.privateKey, encrypted);
         const address = EthCrypto.publicKey.toAddress(bob.publicKey);
-        const res = repo.checkValidation(address, decryptedText);
+        const res = await repo.checkValidation(address, decryptedText);
         expect(res).toBe(true);
-        const res2 = repo.checkValidation(address, decryptedText);
+        const res2 = await repo.checkValidation(address, 'asdasd');
         expect(res2).toBe(false);
     });
 }); 
