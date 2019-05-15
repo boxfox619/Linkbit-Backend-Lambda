f539f04901f1e256d3d388c343040b73aa70d074
boxfox619 <boxfoxsg619@gmail.com>
2019-05-16 08:53:45 +0900

    Fix the address, certification tests

--- handler/__tests__/address.test.ts
+++ handler/__tests__/address.test.ts
@@ -1,8 +1,15 @@
 import { getLinkAddress } from '../address';
 import { apiGatewayEventMock, contextMock } from '../../util/mock';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import sinon from 'sinon';
+import AWS from 'aws-sdk';
 
 describe('address handler', () => {
+    const sandbox = sinon.createSandbox();
+    beforeAll(() => {
+        sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'get').returns({promise: () => ({})});
+    });
+
     it('should return status code 404 with message', async () => {
         const event: APIGatewayProxyEvent = apiGatewayEventMock();
         const context: Context = contextMock();
