6db7cafa894d885902b710e54b1906b1f1e7c944
boxfox619 <boxfoxsg619@gmail.com>
2019-05-19 10:35:51 +0900

    Update gitignore & write address create test

--- handler/__tests__/address.test.ts
+++ handler/__tests__/address.test.ts
@@ -1,4 +1,4 @@
-import { getLinkAddress } from '../address';
+import { getLinkAddress, createAddress, linkAddress } from '../address';
 import { apiGatewayEventMock, contextMock } from '../../util/mock';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import sinon from 'sinon';
@@ -6,8 +6,10 @@ import AWS from 'aws-sdk';
 
 describe('address handler', () => {
     const sandbox = sinon.createSandbox();
+    
     beforeAll(() => {
         sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'get').returns({promise: () => ({})});
+        sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'delete').returns({promise: () => ({})});
     });
 
     it('should return status code 404 with message', async () => {
@@ -22,4 +24,17 @@ describe('address handler', () => {
         expect(body.message).toBeDefined();
         expect(body.message).toBe('not linked address : test, symbol : ETH');
     });
+
+    it('should return status code 400 with message', async () => {
+        const event: APIGatewayProxyEvent = apiGatewayEventMock();
+        const context: Context = contextMock();
+        event.body = JSON.stringify({ linkaddress: 'test', ownerAddress: 'addr2', token: 'token-11' });
+        const res = await createAddress(event, context, () => {});
+        expect(res).toBeDefined();
+        const response = res as APIGatewayProxyResult;
+        const body = JSON.parse(response.body);
+        expect(response.statusCode).toBe(400);
+        expect(body.message).toBeDefined();
+        expect(body.message).toBe('invalid certification token');
+    }, 100000);
 });
