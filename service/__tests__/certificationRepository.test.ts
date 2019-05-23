c30af94ef492a3368a68dd5b99c7fc58f1e31544
Boxfox <boxfoxsg619@gmail.com>
2019-05-23 14:57:47 +0900

    Recover pub to address logic with eth-js-util

--- service/__tests__/certificationRepository.test.ts
+++ service/__tests__/certificationRepository.test.ts
@@ -11,13 +11,13 @@ describe('certificationCache', () => {
     const dbClient = new AWS.DynamoDB.DocumentClient();
     const repo = new CertificationRepository(dbClient);
     const privateKey = crypto.randomBytes(32);
-    const publicKey = eutil.privateToPublic(privateKey).toString('hex');
+    const publicKey = eutil.privateToPublic(privateKey);
     let token: string = undefined;
 
     it('should generate encrypted text', async () => {
         const updateQuery = () => putQuery = putStub.getCall(0).args[0];
         const putStub = sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'put').returns({ promise: updateQuery});
-        token = await repo.createCertText(publicKey);
+        token = await repo.createCertText(publicKey.toString('hex'));
         expect(token).toBeDefined();
     });
 
@@ -25,9 +25,10 @@ describe('certificationCache', () => {
         sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'get').returns({ promise: () => putQuery });
         sandbox.stub(AWS.DynamoDB.DocumentClient.prototype, 'delete').returns({ promise: () => { } });
         const decryptedText = ECIES.decrypt(privateKey, Buffer.from(token, 'hex')).toString();
-        const res = await repo.checkValidation(publicKey, decryptedText);
+        const address = eutil.pubToAddress(publicKey).toString("hex");
+        const res = await repo.checkValidation(address, decryptedText);
         expect(res).toBe(true);
-        const res2 = await repo.checkValidation(publicKey, 'asdasd');
+        const res2 = await repo.checkValidation(address, 'asdasd');
         expect(res2).toBe(false);
     });
 }); 
--- service/certificationRepository.ts
+++ service/certificationRepository.ts
@@ -1,17 +1,19 @@
 import { CertificationUsecase } from '../domain/certification';
 import { Token } from '../models/token';
 import ECIES from 'eth-ecies';
+import * as eutil from 'ethereumjs-util';
 import * as crypto from 'crypto';
 
 export class CertificationRepository implements CertificationUsecase {
 
     constructor(private dbClient: AWS.DynamoDB.DocumentClient) { }
 
-    async createCertText(publicKey: string) {
+    async createCertText(publicKeyHex: string) {
+        const publicKey = Buffer.from(publicKeyHex, 'hex');
         const randomText = crypto.randomBytes(30).toString('hex');
-        //const address = ethCrypto.publicKey.toAddress(publicKey);
-        const encryptedText = ECIES.encrypt(Buffer.from(publicKey, 'hex'), randomText).toString('hex');
-        await this.dbClient.put(new Token(publicKey, randomText).putQuery).promise();
+        const address = eutil.pubToAddress(publicKey).toString("hex");
+        const encryptedText = ECIES.encrypt(publicKey, randomText).toString('hex');
+        await this.dbClient.put(new Token(address, randomText).putQuery).promise();
         return encryptedText;
     }
 
