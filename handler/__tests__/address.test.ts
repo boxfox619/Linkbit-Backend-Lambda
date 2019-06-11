5ca3bd002b3feb9860baa95dfc75eea827ee0476
boxfox619 <boxfoxsg619@gmail.com>
2019-06-11 22:26:30 +0900

    Write the delete address test code

--- handler/__tests__/address.test.ts
+++ handler/__tests__/address.test.ts
@@ -50,6 +50,49 @@ describe('create address handler', () => {
     });
 });
 
+describe('delete address handler', () => {
+    const address = 'address';
+    const token = new Token(address, 'token');
+    const addressRepo = sinon.createStubInstance(AddressRepository);
+    const certRepo = sinon.createStubInstance(CertificationRepository);
+    const addressHanders = AddressHanders(addressRepo, certRepo);
+
+    it('should return status code 400 with message', async () => {
+        certRepo.checkValidation.returns(false);
+        const event: APIGatewayProxyEvent = apiGatewayEventMock({ linkaddress: 'test', token: 'token-11' });
+        const res = await addressHanders.deleteAddress(event, contextMock(), () => { });
+        expect(res).toBeDefined();
+        const response = res as APIGatewayProxyResult;
+        const body = JSON.parse(response.body);
+        expect(response.statusCode).toBe(400);
+        expect(body.message).toBeDefined();
+        expect(body.message).toBe('invalid certification token');
+    });
+
+    it('should return status code 200 and database update', async () => {
+        certRepo.checkValidation.returns(true);
+        const linkaddress = 'test-link-address';
+        const event: APIGatewayProxyEvent = apiGatewayEventMock({ linkaddress, token: token.token });
+        const res = await addressHanders.deleteAddress(event, contextMock(), () => { });
+        expect(res).toBeDefined();
+        const response = res as APIGatewayProxyResult;
+        expect(response.statusCode).toBe(200);
+        expect(addressRepo.deleteAddress.getCalls().length).toBe(1);
+        const deleteAddressCall = addressRepo.deleteAddress.getCall(0);
+        expect(deleteAddressCall.args[0]).toBe(linkaddress);
+    });
+
+    it('should return status code 400 with error', async () => {
+        const event: APIGatewayProxyEvent = apiGatewayEventMock();
+        const res = await addressHanders.deleteAddress(event, contextMock(), () => { });
+        expect(res).toBeDefined();
+        const response = res as APIGatewayProxyResult;
+        const body = JSON.parse(response.body);
+        expect(response.statusCode).toBe(400);
+        expect(body.message).toBe('missing body keys token,linkaddress');
+    });
+});
+
 describe('get link address handler', () => {
     const addressRepo = sinon.createStubInstance(AddressRepository);
     const certRepo = sinon.createStubInstance(CertificationRepository);
